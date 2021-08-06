b71f193a13712508db035b78fe53179334f04530
Cami06101981 <Cami06101981>
2021-08-06 18:38:54 +0000

    approved

--- Nutter/test_code1_code2.py
+++ Nutter/test_code1_code2.py
@@ -1,5 +1,5 @@
 # Databricks notebook source
-# install nutter in cluster if you want to trigger tests from command lines
+# install nutter in cluster if you want to trigger tests from command line
 %python
 %pip install -U nutter
 
